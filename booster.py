6db1211da09b39437d33086d6eb2409d717f6c79
zahra <zahraa.alizadeh@outlook.com>
2024-11-22 12:57:59 +1030

    Handle output dir and template configs
    After this change, the path to the project
    can be configured in the config file.
    
    If the path doesn't exists it creates the path.
    If the path exists, it prompts the user to confirm
    a fresh start or abort.

--- booster.py
+++ booster.py
@@ -1,3 +1,4 @@
+import shutil
 import subprocess
 from pathlib import Path
 
@@ -5,6 +6,8 @@ import typer
 import yaml
 
 app = typer.Typer()
+
+BASE_DIR = Path(__file__).resolve().parent
 
 
 def load_config(config_path: Path):
@@ -17,16 +20,35 @@ def load_config(config_path: Path):
 def django_start_project(config):
     """Create a Django project using the specified configuration."""
     project_name = config["project_name"]
+    template_path = config.get("template_path", "template/")
+    output_dir = Path(config.get("output_dir")) or BASE_DIR / "output"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    project_path = output_dir / project_name
+    if project_path.exists():
+        typer.echo(f"Directory '{project_path}' already exists.")
+        if typer.confirm(
+            "Do you want to clear this directory and start fresh?", default=False
+        ):
+            # Delete the existing directory
+            shutil.rmtree(project_path)
+            typer.echo(f"Cleared the directory: {project_path}")
+        else:
+            typer.echo("Aborting project creation.")
+            raise SystemExit(1)
+    project_path.mkdir(parents=True, exist_ok=True)
 
     # Call django-admin startproject
-    typer.echo(f"Starting Django project '{project_name}'...")
+    typer.echo(BASE_DIR)
+    typer.echo(f"Starting Django project '{project_name}' in {project_path}...")
+
     subprocess.run(
         [
             "django-admin",
             "startproject",
             project_name,
+            str(project_path),
             "--template",
-            "template/",
+            template_path,
             "--extension",
             "py,env,sh,toml,yml",
             "--exclude",
